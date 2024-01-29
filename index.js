1e7f73583d40498dffcc0db740d6e847aa95d103
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-29 17:56:47 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(mondayActivity = "go to the office"){
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(wrapChar){
+    return function (adj) {
+        return `You are ${wrapChar}${adj}${wrapChar}!`;
+    }
+}
